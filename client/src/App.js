00e190abe4c1cac0b20301f59041c316963047a8
Maxson Green <maxson.green@gmail.com>
2021-06-22 10:57:43 -0400

    Added router and created NavBar and Not-Found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/search" component={Search} />
+          <Route path="/saved" component={Saved} />
+          <Redirect from="/" exact to="/search" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </>
   );
 }
 
