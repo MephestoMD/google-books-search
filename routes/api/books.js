f1205e30071b785bf036601fe7963004707288ac
Maxson Green <maxson.green@gmail.com>
2021-06-22 11:08:15 -0400

    Defined routes in the controller

---
+++ routes/api/books.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// /api/books GET route to display all saved books and post to save a book
+router.route("/").get(booksController.findAll).post(booksController.save);
+
+// /api/books/:id to find a book by specific book or delete a specific book from a collection
+router
+  .route("/:id")
+  .get(booksController.findOne)
+  .delete(booksController.remove);
+
+module.exports = router;
